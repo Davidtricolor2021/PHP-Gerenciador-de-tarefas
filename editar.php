5f22501e6d2b8e93eede94d846a987b1b1cbdba9
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-04-26 10:49:03 -0300

    Create editar.php

---
+++ editar.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+require "banco.php";
+require "ajudandes.php";
+
+$exibir_tabela = false;
+
+if (array_key_exists('nome', $_POST) && $_POST['nome'] != '') {
+    $tarefa = [
+        'id' => $_POST['id'],
+        'nome' => $_POST['nome'],
+        'descricao' => '',
+        'prazo' => '',
+        'prioridade' => $_POST['prioridade'],
+        'concluida' => 0,
+    ];
+
+    if (array_key_exists('descricao', $_POST)) {
+        $tarefa['descricao'] = $_POST['descricao'];
+    }
+
+    if (array_key_exists('prazo', $_POST)) {
+        $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);    
+    }
+
+    if (array_key_exists('concluida', $_POST)) {
+        $tarefa['concluida'] = $_POST['concluida'];
+    }
+
+    editar_tarefa($conexao, $tarefa);
+    header('Location: tarefas.php');
+    die();
+}
+
+$tarefa = buscar_tarefa($conexao, $_POST['id']);
+
+require "template.php";
