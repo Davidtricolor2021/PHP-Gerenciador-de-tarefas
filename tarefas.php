3247b2a2cea1f529354067799c795c715ac67d55
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-04-27 10:26:05 -0300

    Update tarefas.php

--- tarefas.php
+++ tarefas.php
@@ -12,7 +12,7 @@ $erros_validacao = [];
 
 if (tem_post()) {
     $tarefa = [
-        'id' => 0,
+        'id' => $_POST['id'],
         'nome' => $_POST['nome'],
         'descricao' => '',
         'prazo' => '',
@@ -20,32 +20,45 @@ if (tem_post()) {
         'concluida' => 0,
     ];
 
+    if (strlen($tarefa['nome']) == 0){
+        $tem_erros = true;
+        $erros_validacao['nome'] = 'O nome da tarefa é obrigatório!';
+    }
+
     if (array_key_exists('descricao', $_POST)) {
         $tarefa['descricao'] = $_POST['descricao'];
     }
 
-    if (array_key_exists('prazo', $_POST)) {
-        $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);    
+    if (array_key_exists('prazo', $_POST) && strlen($_POST['prazo']) > 0) {
+        if (validar_data($_POST['prazo'])) {
+            $tarefa['prazo'] = traduz_data_para_banco($_POST['prazo']);
+        } else {
+            $tem_erros = true;
+            $erros_validacao['prazo'] = 'O prazo não é uma data válida';
+        }
     }
 
     if (array_key_exists('concluida', $_POST)) {
         $tarefa['concluida'] = $_POST['concluida'];
     }
 
-    gravar_tarefa($conexao, $tarefa);
-    header('Location: tarefas.php');
-    die();
+    if (! $tem_erros) {
+        gravar_tarefa($conexao, $tarefa);
+        header('Location: tarefas.php');
+        die();
+    }
 }
 
 $lista_tarefas = buscar_tarefas($conexao);
 
 $tarefa = [
     'id' => 0,
-    'nome' => '',
-    'descricao' => '',
-    'prazo' => '',
-    'prioridade' => 1,
-    'concluida' => ''
+    'nome' => $_POST['nome'] ?? '',
+    'descricao' => $_POST['descricao'] ?? '',
+    'prazo' => (isset($_POST['prazo']))
+        ? traduz_data_para_banco($_POST['prazo']) : '',
+    'prioridade' => $_POST['prioridade'] ?? '',
+    'concluida' => $_POST['concluida'] ??  ''
 ];
 
 require "template.php";
